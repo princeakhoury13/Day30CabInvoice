cef13059354ecefc37347a7564fa2b64e0ad80c4
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-15 21:23:36 +0530

    added: user model

---
+++ CabFareDay30/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabFareDay30
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public List<Ride> Rides { get; set; }
+
+        public User(int userId)
+        {
+            UserId = userId;
+            Rides = new List<Ride>();
+        }
+    }
+}

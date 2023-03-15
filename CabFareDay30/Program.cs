0e8a33cf9d758031999ee091bc493505e8c1c0e7
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-15 20:51:18 +0530

    added: Step one to calculate cab fare

---
+++ CabFareDay30/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CabFareDay30
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double distance, time, fare;
+
+            Console.WriteLine("Enter the distance travelled (in km): ");
+            distance = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the time taken (in minutes): ");
+            time = Convert.ToDouble(Console.ReadLine());
+
+            fare = (distance * 10) + (time * 1);
+
+            if (fare < 5)
+            {
+                fare = 5;
+            }
+
+            Console.WriteLine("Total fare: Rs. " + fare);
+        }
+    }
+}

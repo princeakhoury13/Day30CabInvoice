a114a2e4c894ea27e6e571f05898c5b82b388d9a
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-15 20:56:26 +0530

    added: Step two to calculate multiple cab fares

--- CabFareDay30/Program.cs
+++ CabFareDay30/Program.cs
@@ -4,22 +4,34 @@
     {
         public static void Main(string[] args)
         {
-            double distance, time, fare;
+            int numRides;
+            double distance, time, fare, totalFare = 0;
 
-            Console.WriteLine("Enter the distance travelled (in km): ");
-            distance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the number of rides: ");
+            numRides = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter the time taken (in minutes): ");
-            time = Convert.ToDouble(Console.ReadLine());
-
-            fare = (distance * 10) + (time * 1);
-
-            if (fare < 5)
+            for (int i = 1; i <= numRides; i++)
             {
-                fare = 5;
+                Console.WriteLine("Ride " + i);
+                Console.WriteLine("Enter the distance travelled (in km): ");
+                distance = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter the time taken (in minutes): ");
+                time = Convert.ToDouble(Console.ReadLine());
+
+                fare = (distance * 10) + (time * 1);
+
+                if (fare < 5)
+                {
+                    fare = 5;
+                }
+
+                Console.WriteLine("Total fare for ride " + i + ": Rs. " + fare);
+
+                totalFare += fare;
             }
 
-            Console.WriteLine("Total fare: Rs. " + fare);
+            Console.WriteLine("Total fare for all rides: Rs. " + totalFare);
         }
     }
 }

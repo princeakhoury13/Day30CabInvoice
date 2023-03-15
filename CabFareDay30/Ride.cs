81e6a202b54328075a0a05da1190c58ff20d72e2
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-15 21:33:14 +0530

    added: normal and premium rides

--- CabFareDay30/Ride.cs
+++ CabFareDay30/Ride.cs
@@ -24,4 +24,31 @@ namespace CabFareDay30
             }
         }
     }
+
+    public class NormalRide : Ride
+    {
+        public NormalRide(double distance, double time) : base(distance, time)
+        {
+            Fare = (distance * 10) + (time * 1);
+
+            if (Fare < 5)
+            {
+                Fare = 5;
+            }
+        }
+    }
+
+    public class PremiumRide : Ride
+    {
+        public PremiumRide(double distance, double time) : base(distance, time)
+        {
+            Fare = (distance * 15) + (time * 2);
+
+            if (Fare < 20)
+            {
+                Fare = 20;
+            }
+        }
+
+    }
 }

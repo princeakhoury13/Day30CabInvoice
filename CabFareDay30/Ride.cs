b6a36ed36014c5d7e51afe50d2f8cf72ca42fce6
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-15 21:23:57 +0530

    added: rides model

---
+++ CabFareDay30/Ride.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabFareDay30
+{
+    public class Ride
+    {
+        public double Distance { get; set; }
+        public double Time { get; set; }
+        public double Fare { get; set; }
+
+        public Ride(double distance, double time)
+        {
+            Distance = distance;
+            Time = time;
+            Fare = (distance * 10) + (time * 1);
+
+            if (Fare < 5)
+            {
+                Fare = 5;
+            }
+        }
+    }
+}
